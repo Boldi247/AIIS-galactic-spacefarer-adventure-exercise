9b0d8923877ce827ed26383b6ede2276da81e0b0
Boldizsár Kovács <kovacs.boldizsar@sonrisa.hu>
2026-09-22 18:07:15 +0200

    integrated resend for email sending, fixed formatting issues

--- srv/lib/notifications.js
+++ srv/lib/notifications.js
@@ -1,3 +1,8 @@
+import { Resend } from 'resend'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+const EMAIL_FROM = 'onboarding@resend.dev'
+
 /**
  * Sends an email notification to a spacefarer.
  * Temporarily, this function logs the email content to the console instead of sending an actual email.
@@ -5,11 +10,31 @@
  * @param {Object} spacefarer - The spacefarer to notify.
  */
 export async function sendEmailNotification(spacefarer) {
+  const isResendApiKeySet = process.env.RESEND_API_KEY && process.env.RESEND_API_KEY.trim() !== ''
+
+  if (!isResendApiKeySet) {
     // eslint-disable-next-line no-console
     console.log(
+      `RESEND API KEY IS NOT SET! - Sending notification trough console:\n\n` +
         `Dear ${spacefarer.name},\n\n` +
         `Welcome to the Galactic Spacefarer Adventure! We are thrilled to have you on board as a spacefarer.\n\n` +
         `Your journey begins with a basic wormhole navigation skill level of ${spacefarer.wormholeNavigationSkill} and a stardust collection of ${spacefarer.stardustCollection}.\n\n` +
-        `Remember to pick up your ${spacefarer.spaceSuitColor} colored space suit before embarking on your interstellar journey!`
-    );
+        `Remember to pick up your ${spacefarer.spaceSuitColor} colored space suit before embarking on your interstellar journey!`,
+    )
+
+    return
+  }
+
+  const { data } = await resend.emails.send({
+    from: EMAIL_FROM,
+    to: 'kovacs.boldizsar0321@gmail.com',
+    subject: 'Welcome to the Galactic Spacefarer Adventure!',
+    text:
+      `Dear ${spacefarer.name},\n\n` +
+      `Welcome to the Galactic Spacefarer Adventure! We are thrilled to have you on board as a spacefarer.\n\n` +
+      `Your journey begins with a basic wormhole navigation skill level of ${spacefarer.wormholeNavigationSkill} and a stardust collection of ${spacefarer.stardustCollection}.\n\n` +
+      `Remember to pick up your ${spacefarer.spaceSuitColor} colored space suit before embarking on your interstellar journey!`,
+  })
+
+  return data
 }
